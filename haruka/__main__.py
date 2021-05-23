76d592660b43860478a3fb4209e3b0d978c67dd6
AlphaTp <83142843+AlphaTp@users.noreply.github.com>
2021-05-23 23:13:37 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,11 +20,11 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {},I am {}!
-a complete group managing bot ,Add me in supergroups and promote as admin.
+PM_START = """Hello {},
+I am {}!a complete group managing bot ,Add me in supergroups and promote as admin.
+
 >> Activate Anti-spam protection in your groups by this command : 
 /antispam on
-
 >>Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
 """
 
